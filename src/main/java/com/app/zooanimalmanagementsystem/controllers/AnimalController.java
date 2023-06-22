b6872491231bf268add058e99582fa1df1388a89
PauliusMaslauskas <paulius.mass@gmail.com>
2023-06-22 20:40:06 +0300

    Refactor endpoint URIs

--- src/main/java/com/app/zooanimalmanagementsystem/controllers/AnimalController.java
+++ src/main/java/com/app/zooanimalmanagementsystem/controllers/AnimalController.java
@@ -3,6 +3,7 @@ package com.app.zooanimalmanagementsystem.controllers;
 import com.app.zooanimalmanagementsystem.DTO.AnimalDTO;
 import com.app.zooanimalmanagementsystem.entities.Animal;
 import com.app.zooanimalmanagementsystem.services.AnimalService;
+import com.app.zooanimalmanagementsystem.services.ZooService;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,33 +16,34 @@ import java.util.List;
 @RestController
 @ControllerAdvice
 @JsonIgnoreProperties
-@RequestMapping("animal")
 public class AnimalController {
 
     @Autowired
     private final AnimalService animalService;
+    private final ZooService zooService;
 
-    @PostMapping("/add")
+    @PostMapping("/animal")
     public Animal addAnimal(@RequestBody AnimalDTO animalDTO) {
+        zooService.accommodateAnimals();
         return animalService.saveAnimal(animalDTO);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("/animals")
     public List<Animal> getAllAnimals(){
         return animalService.getAllAnimals();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/animal/{id}")
     public Animal getAnimal(@PathVariable("id") int id){
         return animalService.findAnimalById(id);
     }
 
-    @PutMapping("/update/{id}")
+    @PutMapping("/animal/{id}")
     public Animal updateAnimal(@RequestBody AnimalDTO animalDTO,@PathVariable("id") int id){
         return animalService.updateAnimal(animalDTO, id);
     }
 
-    @DeleteMapping ("/delete/{id}")
+    @DeleteMapping ("/animal/{id}")
     public void deleteAnimal(@PathVariable("id") int id){
         animalService.deleteAnimal(id);
     }
